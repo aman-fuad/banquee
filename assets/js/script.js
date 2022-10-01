832e08ca083d46802d3b3bbb483b1777cbdffab5
aman-fuad <amanullahfuad@gmail.com>
2022-10-02 01:21:19 +0600

    added accordion and footer, fixed some bug/issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
+const nav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
 
-// $('.quote-slider').slick({
-//     autoplay: true,
-//     dots: true,
-//     prevArrow: false,
-//     nextArrow: false
-// });
+navToggle.addEventListener("click", () => {
+    
+    const visiblity = nav.getAttribute("data-visible");
+    if (visiblity === "false") {
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
+    } else {
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
+    }
+});
